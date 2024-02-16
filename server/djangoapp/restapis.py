cc9b9b238b637e2e87487f16c7db30d90403c076
Vince <vincentm88@gmail.com>
2024-02-16 11:49:55 -0500

    fixing lint 4

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,4 +1,6 @@
 # Uncomment the imports below before you add the function code
+
+
 import requests
 import os
 from dotenv import load_dotenv
@@ -11,13 +13,14 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if(kwargs ):
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
     print(backend_url)
@@ -32,8 +35,10 @@ def get_request(endpoint, **kwargs):
         # If any error occurs
         print("Network exception occurred", e)
 
+
 # def analyze_review_sentiments(text):
 def analyze_review_sentiments(text):
+
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
         # Call get method of requests library with URL and parameters
@@ -43,13 +48,16 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 # def post_review(data_dict):
 # Add code for posting review
 def post_review(data_dict):
+
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
