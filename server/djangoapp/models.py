2e257f1a6f583d0f92fbe2d2fd19d44b20809e56
Vince <vincentm88@gmail.com>
2024-02-16 11:33:03 -0500

    fixing lint 2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -30,7 +29,7 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 class CarModel(models.Model):
-    
+
     # Many-to-One relationship
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
@@ -53,10 +52,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=15, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2024),
-            MinValueValidator(2012)
-        ])
+                            validators=[
+                                MaxValueValidator(2024),
+                                MinValueValidator(2012)
+                            ])
     # Other fields as needed
 
     def __str__(self):
