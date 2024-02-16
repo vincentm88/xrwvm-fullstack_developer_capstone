075066e887c2f27fe5d16f113821f7d738d15a46
Vince <vincentm88@gmail.com>
2024-02-16 11:41:09 -0500

    fixing lint 3

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -52,10 +52,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=15, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-                            validators=[
-                                MaxValueValidator(2024),
-                                MinValueValidator(2012)
-                            ])
+                                validators=[
+                                    MaxValueValidator(2024),
+                                    MinValueValidator(2012)
+                                ])
     # Other fields as needed
 
     def __str__(self):
